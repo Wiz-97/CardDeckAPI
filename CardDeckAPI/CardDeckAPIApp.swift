86f85911e0435cbf45ccc971f865c4b4eabda561
Wiz-97 <samuel97316@gmail.com>
2025-02-03 14:44:55 +0900

    Initial Commit

---
+++ CardDeckAPI/CardDeckAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardDeckAPIApp.swift
+//  CardDeckAPI
+//
+//  Created by Samuel Lee on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CardDeckAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
